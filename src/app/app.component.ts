0e35b2702a075aeaf8d0ade4e2f8cecdb07f31d3
SaintSteven <estebandiazaranda@gmail.com>
2023-03-07 03:05:25 -0300

    [FEAT] Added flag counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,13 @@ import { Cell } from '../game/cell';
 export class AppComponent {
   title = 'Buscaminas EY';
   board!: Board;
+  totalFlagsInBoard: number;
+  totalBombsInBoard: number;
 
   constructor() {
     this.reset();
+    this.totalFlagsInBoard = 0;
+    this.totalBombsInBoard = 20 - Math.floor(Math.random() * 10);
   }
 
   checkCell(cell: Cell) {
@@ -22,16 +26,20 @@ export class AppComponent {
       alert('you win');
     }
   }
+
   flag(cell: Cell) {
     if (cell.status === 'flag') {
       cell.status = 'open';
+      this.totalFlagsInBoard--;
     } else if (cell.status === 'open') {
       cell.status = 'flag';
+      this.totalFlagsInBoard++;
     }
   }
 
   reset() {
     const totalBombs = 20 - Math.floor(Math.random() * 10)
     this.board = new Board(10, totalBombs);
+    this.totalFlagsInBoard = 0;
   }
 }
